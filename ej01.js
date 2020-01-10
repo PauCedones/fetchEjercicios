1d90fdecd0f691334c7fdeddec14ed7cff17ef02
Paula <cedones@gmail.com>
2020-01-10 11:14:09 -0300

    no esta funcionando

--- ej01.js
+++ ej01.js
@@ -17,6 +17,46 @@ const getTodos = () =>{
     .catch(err => console.log(err));
 }
 
+// HACER UNA FUNCION QUE AGARRE LOS DATOS Y LOS MUESTRE, LOS 200 DATOS
+
+const mostrarLista = () =>{
+    const ul = document.querySelector("#todo-list");
+    //crear cada elemento, cada span y los botones
+    //se usa un bucle para que se cree por cada item de la lista
+    for(let todo of list){
+        const li = document.createElement("li");
+        const title = document.createElement("span");
+        const eliminar = document.createElement("button");
+        // crear lo que dice adentro del boton
+        eliminar.innerText = "Eliminar";
+        //poner clases a los elementos creados
+        li.className = "todo-item";
+        //otra forma de poner la clase
+        title.setAttribute("class","todo-title");
+        //creamos otro titulo
+        const titleText = document.createElement(todo.title);
+
+        //vinculamos cada elemento
+        title.appendChild(titleText);
+        title.innerText = todo.title;
+
+        //agregamos un evento al boton de eliminar
+        eliminar.addEventListener("click", ()=>{
+            deleteTodo(todo.id).then(mostrarLista)
+        });
+
+        //vinculamos
+        li.appendChild(title);
+        ul.appendChild(li);
+    }
+
+    getTodos().then(mostrarLista);
+    
+// vincular cada elemtno
+//esta funcion se llama desde dentro de la funcion de getTodos!!!!!!!
+
+
+
 //02
 
 let usuario;
@@ -26,8 +66,6 @@ const getTodo = (id) =>{
         usuario = res.data;
     })
     .catch(err => console.log(err));
-
-
 }
 
 //03
@@ -79,15 +117,4 @@ const deleteTodo = (id) => {
         list.splice(index,1);
     })
     .catch(err => {throw err;});
-}
-
-////////////////////
-
-// HACER UNA FUNCION QUE AGARRE LOS DATOS Y LOS MUESTRE, LOS 200 DATOS
-
-const mostrarLista = () =>{
-    let lista = document.querySelector("#todo-list");
-//crear cada elemento, cada span y los botones
-// vincular cada elemtno
-//esta funcion se llama desde dentro de la funcion de getTodos!!!!!!!
-document.createElement
+}}
