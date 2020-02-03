bc07115406c82202b623f6ce17cd4919dc8b4bdb
Paula <cedones@gmail.com>
2020-02-02 21:06:19 -0300

    funionaba mostrar la lista y ahora no

--- ej01.js
+++ ej01.js
@@ -7,73 +7,95 @@
 // USAR LA LISTA DE TODOS
 
 //01 
-let list = []
+let lista = [];
+let todo = {
+    id: null,
+    title: "",
+    userId: null,
+    completed: false
+};
+
+const todoUl = document.querySelector("#todo-list");
+
+
+const mostrarLista = (list) =>{
+    todoUl.innerHTML=`
+    <li>
+    <span>ID</span>
+    <span>Tarea</span>
+    <span>Usuario</span>
+    <span>Completada</span>
+    <span>Eliminar</span>
+    </li>`;
+
+    //crear cada elemento, cada span y los botones
+    //se usa un bucle para que se cree por cada item de la lista
+    for(let todo of list){
+        //creando los elementos a imprimir
+        let id = document.createElement("span");
+        let title = document.createElement("span");
+        let idUser = document.createElement("span");
+        let status = document.createElement("span");  
+
+        //aca estamos diciendo que es lo que va a estar adentro de cada item
+        // la variable todo es el elemento de la lista de array
+        // id, tittle, idUser, completed son items dentro del array que tienen ese nombre para identificarlos
+        id.innerHTML = todo.id;
+        id.className = "todo-id";
+        title.innerHTML = todo.tittle; 
+        tittle.className = "todo-tittle";
+        idUser.innerHTML = todo.userId;
+        idUser.className = "todo-idUser";
+        status.innerHTML = todo.completed;
+        status.className = "todo-completed";
+
+        //BOTON DE ELIMINAR EN C/ELEMENTO
+        //creando el boton
+        let eliminar = document.createElement("button");
+        // poniendo que dice adentro del boton
+        eliminar.innerText = "Eliminar";
+        //creando el evento de eliminar cada item del array
+        eliminar.addEventListener("click",()=>{
+            let li = eliminar.parentElement;
+            li.parentNode.removeChild(li);
+            deleteTodo(todo.id);
+        });
+
+        let li = document.createElement("li");
+        li.className = "todo-item";
+
+        //vinculamos
+        li.appendChild(id);
+        li.appendChild(tittle);
+        li.appendChild(userId);
+        li.appendChild(status);
+        li.appendChild(eliminar);
+        if(todo.completed){
+            li.classList.add("listo");
+        }
+        todoUl.appendChild(li);
+    }
+};
 
 const getTodos = async () =>{
     try{
         const res = await axios.get("https://jsonplaceholder.typicode.com/todos");
         //el console.log es a modo ejem
-        list=res.data;
+        lista=res.data;
         //la funcion de abajo invocada aca!
-        mostrarLista();
+        mostrarLista(lista);
     } catch (err){
         err =>{
             alert(`Hubo un error. ${err}`);
         }
     }
 }
-    
-   // .then(res => {
-     //   list = res.data;
-    //})
-    //.catch(err => console.log(err));
-//}
+
+getTodos();
 
 // HACER UNA FUNCION QUE AGARRE LOS DATOS Y LOS MUESTRE, LOS 200 DATOS
 
-const mostrarLista = () =>{
-    const ul = document.querySelector("#todo-list");
-    //reinicia la lista, sino cada vez que estuviesemos poniendo algo nuevo se imprimiria abajo de vuelta
-    ul.innerHTML=""
-    //crear cada elemento, cada span y los botones
-    //se usa un bucle para que se cree por cada item de la lista
-    for(let todo of list){
-        const li = document.createElement("li");
-        const title = document.createElement("span");  
-        const eliminar = document.createElement("button");
-        // crear lo que dice adentro del boton
-        eliminar.innerText = "Eliminar";
-        //poner clases a los elementos creados
-        li.className = "todo-item";
-        //otra forma de poner la clase
-        title.setAttribute("class","todo-title");
-        //creamos un nodo de texto, agregando texto al span
-        const titleText = document.createTextNode(todo.title);
 
-        //vinculamos cada elemento
-        title.appendChild(titleText);
-        title.innerText = todo.title;
-
-        //agregamos un evento al boton de eliminar
-        eliminar.addEventListener("click", ()=>{
-            deleteTodo(todo.id);
-        });
-
-        //vinculamos
-        li.appendChild(title);
-        ul.appendChild(li);
-    }
-}
-    getTodos();
-
-    //getTodos().then(mostrarLista);
-    /*
-    Se hace con .then es porque 
-    la primera función devuelve una promesa, 
-    entonces el then es para que la segunda 
-    sepa que debe esperar que se cumpla 
-    la promesa para ejecutarse
-    */
 
 
 //02
